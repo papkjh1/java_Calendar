21189f591d69c8e4f41b39bafbd21cacfc88d8ec
papkjh1 <papkjh1@DESKTOP-1AMNMRT>
2020-11-12 23:50:41 +0900

    Fix encoding problem

---
+++ src/calendar/Sum.java
@@ -0,0 +1,15 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("두 수를 입력하세요.");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("두 수의 합은 "+(a+b)+" 입니다.");
+	}
+
+}
