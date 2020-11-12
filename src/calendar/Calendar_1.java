a190ff85123d8b77ff1bd8b792dd621d652c07d7
papkjh1 <papkjh1@DESKTOP-1AMNMRT>
2020-11-12 23:46:45 +0900

    Fix encoding problem

--- src/calendar/Calendar_1.java
+++ src/calendar/Calendar_1.java
@@ -3,7 +3,8 @@ package calendar;
 public class Calendar_1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,Calendarㅎ");
+		System.out.println("Hello,Calendar");
+		System.out.println("한글테스트");
 	}
 
 }
