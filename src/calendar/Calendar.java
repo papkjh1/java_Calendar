c74d17fb2783a95996aacda8ff37c0b707e8e15f
papkjh1 <papkjh1@DESKTOP-1AMNMRT>
2020-11-13 00:15:46 +0900

    Update

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,14 +3,17 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-
+	private static final int[] MAX_DAYS= {0,31,28,31,30,31,30,31,31,30,31,30,31};
+	public int getmaxDaysOfMonth(int month) {
+		return MAX_DAYS[month];
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");
 		int month = sc.nextInt();
 		
-		int [] maxDays = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-		System.out.println(month+"의 달은"+maxDays[month]+"일까지 있습니다.");
+		System.out.println(month+"의 달은"+cal.getmaxDaysOfMonth(month)+"일까지 있습니다.");
 		sc.close();
 	}
 
