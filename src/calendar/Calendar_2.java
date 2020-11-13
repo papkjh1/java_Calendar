16bfc8e65d5d39a4d20eb16bfe95dfcf24edf3cf
papkjh1 <papkjh1@DESKTOP-1AMNMRT>
2020-11-13 12:28:32 +0900

    Update

--- src/calendar/Calendar_2.java
+++ src/calendar/Calendar_2.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class Calendar_2 extends Calendar{
 
 	public static void main(String[] args) {
-		
+		Calendar cal = new Calendar();
 		Scanner sc = new Scanner(System.in);
-		int a,b,c;
 		System.out.println("반복횟수를 입력하세요");
-		a=sc.nextInt();
+		int repeat =sc.nextInt();
 		System.out.println("월을 입력하세요");
-		for (int i = 0; i < a; i++) {
-		
+		for (int i = 0; i < repeat; i++) {
+			int month = sc.nextInt();
+			System.out.printf("%d월은 %d월까지 있습니다",month,cal.getmaxDaysOfMonth(month));
 		}
-		
 		
 		
 		
