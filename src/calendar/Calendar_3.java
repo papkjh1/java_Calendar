a764da400cda983aeeaa27ed0dc80029e86837cb
papkjh1 <papkjh1@DESKTOP-1AMNMRT>
2020-11-13 13:42:47 +0900

    Update

---
+++ src/calendar/Calendar_3.java
@@ -0,0 +1,26 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Calendar_3 extends Calendar {
+
+	public static void main(String[] args) {
+		String PROMPT = "cal> ";
+		Calendar cal = new Calendar();
+		Scanner sc = new Scanner(System.in);
+
+		int month = 0;
+		while (true) {
+			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
+			month = sc.nextInt();
+			System.out.printf("%d달은 %d월까지 있습니다", month, cal.getmaxDaysOfMonth(month));
+			if (month <= 0 && month>12) {
+				break;
+			}
+		}
+		sc.close();
+		System.out.println("bye~");
+	}
+
+}
