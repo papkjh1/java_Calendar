ec3e946b4a9cd43920a3d105f59af8691820520b
papkjh1 <papkjh1@DESKTOP-1AMNMRT>
2020-11-13 09:23:22 +0900

    Update

---
+++ src/calendar/Calendar_2.java
@@ -0,0 +1,23 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Calendar_2 extends Calendar{
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("반복횟수를 입력하세요");
+		a=sc.nextInt();
+		System.out.println("월을 입력하세요");
+		for (int i = 0; i < a; i++) {
+		
+		}
+		
+		
+		
+		
+	}
+
+}
